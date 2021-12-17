11e94476310569d54f71e19a352eb74c34c9a3ec
Lucas-Gardini <lucasgardini.dias@hotmail.com>
2021-12-17 14:38:03 -0300

    Added GET and PUT to enderecos

--- src/enderecos/enderecos.controller.ts
+++ src/enderecos/enderecos.controller.ts
@@ -1,10 +1,10 @@
 import {
 	Controller,
 	Get,
-	Param,
 	Body,
 	Post,
 	Put,
+	Param,
 	Delete,
 } from "@nestjs/common";
 import { EnderecosService } from "./enderecos.service";
@@ -19,8 +19,26 @@ export class EnderecosController {
 		return this.enderecosService.getAll();
 	}
 
+	@Get("/:id")
+	async getById(@Param("id") id: string): Promise<Endereco> {
+		return this.enderecosService.getById(id);
+	}
+
 	@Post()
 	async create(@Body() endereco: Endereco): Promise<Endereco> {
 		return this.enderecosService.create(endereco);
+	}
+
+	@Put("/:id")
+	async update(
+		@Param("id") id: string,
+		@Body() pessoa: Endereco,
+	): Promise<Endereco> {
+		return this.enderecosService.update(id, pessoa);
+	}
+
+	@Delete("/:id")
+	async delete(@Param("id") id: string): Promise<any> {
+		return this.enderecosService.delete(id);
 	}
 }
